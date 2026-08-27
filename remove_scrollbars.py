d0f715954d379266ed3fa8f1f1a7339f4b341614
Thanoj Buddhima <thanojbuddhima2003@gmail.com>
2026-08-27 13:50:59 +0530

    Visually hide all scrollbars while maintaining scroll functionality

---
+++ remove_scrollbars.py
@@ -0,0 +1,36 @@
+import os
+
+directory = "/Users/thanojbuddhima/Development/python-lessons"
+
+def update_file(filepath):
+    with open(filepath, 'r') as f:
+        content = f.read()
+
+    original_content = content
+    
+    css_addition = """
+  /* Hide scrollbars globally but keep scrollability */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  * {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+"""
+    if '::-webkit-scrollbar {' not in content:
+        content = content.replace('</style>', css_addition + '</style>')
+
+    if content != original_content:
+        with open(filepath, 'w') as f:
+            f.write(content)
+        return True
+    return False
+
+updated = 0
+for filename in os.listdir(directory):
+    if filename.endswith(".html") and filename != "index.html":
+        if update_file(os.path.join(directory, filename)):
+            updated += 1
+            
+print(f"Updated {updated} files.")
